fd2b21b13b430aadf488149705f00cc0c8f5411e
s2107747 <s2107747@student.rmit.edu.au>
2017-08-08 13:14:16 +1000

    original version. does'nt fade

--- LIndicator.java
+++ LIndicator.java
@@ -7,16 +7,9 @@ public class LIndicator extends Indicator {
 
 	@Override
 	public void display() throws InterruptedException {
-		String colorInd;
-		int i;
-		for(i=0;i<10;i++)
-		{
-		colorInd="\033["+i*3+"m<-\033[0m\r";
-		//System.out.println(color);
-		System.out.print("\033[32m<-\033[0m\r");
-		Thread.sleep(50);
-		}
-		System.out.print("             \r");
+		System.out.print("<-\r");
+		Thread.sleep(500);
+		System.out.print("   \r");
 		Thread.sleep(500);
 		
 	}
