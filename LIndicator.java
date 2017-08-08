c6ce8e003d6e068a7495b219c6e665df657475f7
s2107747 <s2107747@student.rmit.edu.au>
2017-08-08 11:39:16 +1000

    experiment with fade 2

--- LIndicator.java
+++ LIndicator.java
@@ -11,7 +11,8 @@ public class LIndicator extends Indicator {
 		int i;
 		for(i=0;i<10;i++)
 		{
-		color="\01["+i*3+"m";
+		color="\033["+i*3+"m";
+		System.out.println(color);
 		System.out.print(color+"<-" + "\033[0m\r");
 		Thread.sleep(50);
 		}
