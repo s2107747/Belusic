27681d4285b179dbbc248f14d027c632b0472b12
s2107747 <s2107747@student.rmit.edu.au>
2017-08-08 11:49:33 +1000

    experiment 5

--- LIndicator.java
+++ LIndicator.java
@@ -12,7 +12,8 @@ public class LIndicator extends Indicator {
 		for(i=0;i<10;i++)
 		{
 		color="\033["+i*3+"m";
-		System.out.print(color+"<-" + "\033[0m\r");
+		//System.out.println(color);
+		System.out.print("\033[32"+"<-" + "\033[0m\r");
 		Thread.sleep(50);
 		}
 		System.out.print("             \r");
