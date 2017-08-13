d973332e8843186ed2287ffe0dac696a270449b0
s3590079 <s3590079@student.rmit.edu.au>
2017-08-13 14:25:10 +0800

    Update EngineTemp.java
    1) Added temperature range from 1 to 100 for simulation purpose.
    2) Added group, user and student id.
    3) Clean up comments and unused codes.

--- EngineTemp.java
+++ EngineTemp.java
@@ -1,3 +1,9 @@
+// Team: NobleBrick
+// Name: Edwin Chow
+// Student ID: s3590079
+// Course: CPT111
+// Date: SP2 2017
+
 import java.util.Scanner;
 // i've removed abstract from class. not neccessary
 public class EngineTemp {
@@ -10,27 +16,36 @@ public class EngineTemp {
 	}
 	//get state of engine temp
 	public boolean isHigh(){return tempHigh;}
-	
+	//Dan's comment:
 	//code below is from edwins engine temp class. I've made it a method of the class 
 	//engineTemp.setTemp(Scanner in)
-   //neccessary to pass Scanner as parameter from main. otherwise it produced error on return to main
-	//not sure why...
+	//neccessary to pass Scanner as parameter from main. otherwise it produced error on return to main
+	//not sure why..
+	
 	public void setTemp(Scanner in){
-    //this code provided by edwin has not been changed, exept for removal of scanner
+    	//this code provided by edwin has not been changed, exept for removal of scanner
             do{ 
                     //Simulating Temperature input
                     System.out.println("Simulating temperature input");
-                    System.out.println("Enter temperature range: [1] to [10]");
+                    System.out.println("Enter temperature in degree Celsius range: [1] to [100]");
+		    System.out.println("[1-20]-COLD");
+		    System.out.println("[21-80]-NORMAL");
+		    System.out.println("[81-100]-HOT");
                     System.out.println("[0] Exit");
                     // Display output
                     choice = in.nextInt();
-                    if (choice >=1  && choice <=7){
-                            System.out.println("Temperature is \033[32mnormal" + "\033[0m");
+		    System.out.println("Temperature checking...");
+                    if (choice >=1 && choice <=20){
+                            System.out.println("Temperature is \033[34mCOLD!" + "\033[0m");
                     		tempHigh=false;}
-                    else if (choice >=8 && choice <=10){
-                            System.out.println("Temperature is \033[31mhot!" + "\033[0m");
-                    tempHigh=true;}
+		    else if (choice >=21 && choice <=80){
+			    System.out.println("Temperature is \033[32mNORMAL!" + "\033[0m");
+			     	tempHigh=false;}
+                    else if (choice >=81 && choice <=100){
+                            System.out.println("Temperature is \033[31mHOT!" + "\033[0m");
+                    		tempHigh=true;}
+		    //dummy line
+		    System.out.println("");
             } while (choice !=0);
-            
     }
 }
